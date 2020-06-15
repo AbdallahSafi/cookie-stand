c41f99b38af1b039733af1c73e9a27bcea02f5de
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-06-15 15:02:18 +0300

    adding funnction that calculae the total cookies per hour

--- js/app.js
+++ js/app.js
@@ -1,58 +1,87 @@
-'use strict';
+"use strict";
 
 // ---------------- Salmon cookie Object ----------------
-function SalmonCookie(times, customerMin, customerMax, avgCookie) {
-  this.times = times;
+var salmonObjs = [];
+function SalmonCookie(customerMin, customerMax, avgCookie) {
+  this.times = [
+    "6:00am",
+    "7:00am",
+    "8:00am",
+    "9:00am",
+    "10:00am",
+    "11:00am",
+    "12:00pm",
+    "1:00pm",
+    "2:00pm",
+    "3:00pm",
+    "4:00pm",
+    "5:00pm",
+    "6:00pm",
+    "7:00pm",
+  ];
   this.customerMin = customerMin;
   this.customerMax = customerMax;
-  this.avgCookie = avgCookie;
   this.customerRandomNums = [];
-  this.sumCustomerRandomNums = 0;
-  this.totalCookies = [];
+  this.avgCookie = avgCookie;
+  this.cookiesRandomNums = [];
+  this.totalCookiesPerLocation = 0;
+  salmonObjs.push(this);
 }
 
 // This function should generat a random number for a given max and min numbers
 SalmonCookie.prototype.getCustomerRandom = function () {
   for (var i = 0; i < this.times.length; i++) {
-    var randomNum = generateRandomCostumerNum(
-      this.customerMin,
-      this.customerMax
-    );
+    var randomNum = generateRandomNum(this.customerMin, this.customerMax);
     //adding to array
     this.customerRandomNums.push(randomNum);
-    // sum all the items
-    this.sumCustomerRandomNums = this.sumCustomerRandomNums + randomNum;
   }
 };
 
+// This function should calculate the cookies avg number for each hour
+SalmonCookie.prototype.getCookiesNum = function () {
+  for (var j = 0; j < this.customerRandomNums.length; j++) {
+    var cookieNum = multiply(this.customerRandomNums[j], this.avgCookie);
+    //adding to array
+    this.cookiesRandomNums.push(cookieNum);
+    // sum all the items
+    this.totalCookiesPerLocation = this.totalCookiesPerLocation + cookieNum;
+  }
+};
 
+// Creating the objects
+var seattle = new SalmonCookie(23, 65, 6.3);
+var tokyo = new SalmonCookie(3, 24, 1.2);
+var dubai = new SalmonCookie(11, 38, 3.7);
+var paris = new SalmonCookie(20, 65, 2.3);
+var lima = new SalmonCookie(2, 16, 4.6);
 
-var seattle = new SalmonCookie(
-  [
-    '6:00am',
-    '7:00am',
-    '8:00am',
-    '9:00am',
-    '10:00am',
-    '11:00am',
-    '12:00pm',
-    '1:00pm',
-    '2:00pm',
-    '3:00pm',
-    '4:00pm',
-    '5:00pm',
-    '6:00pm',
-    '7:00pm',
-  ],
-  23,
-  65,
-  6.3
-);
+for (var d = 0; d < salmonObjs.length; d++) {
+  salmonObjs[d].getCustomerRandom();
+  salmonObjs[d].getCookiesNum();
+}
 
-seattle.getCustomerRandom();
-console.log(seattle.customerRandomNums);
-console.log(seattle.sumCustomerRandomNums);
+//Calculating the Total of cookies per hour
+function getTotalPerhour() {
+  var totalPerHour = [];
+  var sumTotalPerHour = 0;
+  for (var z = 0; z < salmonObjs[0].times.length; z++) {
+    var perHour = 0;
+    for (var y = 0; y < salmonObjs.length; y++) {
+      perHour = perHour + salmonObjs[y].cookiesRandomNums[z];
+    }
+    totalPerHour.push(perHour);
+    sumTotalPerHour = sumTotalPerHour + perHour;
+  }
+}
 
+// console.log(seattle.cookiesRandomNums);
+// console.log(tokyo.cookiesRandomNums);
+// console.log(dubai.cookiesRandomNums);
+// console.log(paris.cookiesRandomNums);
+// console.log(lima.cookiesRandomNums);
+
+// console.log(totalPerHour);
+// console.log(sumTotalPerHour);
 
 // SalmonCookie.prototype.getCookiesNum = function () {
 //   this.cookiesRandomNum = multiplyCookieCostumer(
@@ -166,14 +195,14 @@ console.log(seattle.sumCustomerRandomNums);
 
 // --------------Functions Section -------------------
 // Generat a random number of  given min and max value
-function generateRandomCostumerNum(min, max) {
+function generateRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// //calculate the product of a given two parameter
-// function multiplyCookieCostumer(costumerNum, cookieNum) {
-//   return Math.floor(costumerNum * cookieNum);
-// }
+//calculate the product of a given two parameter
+function multiply(a, b) {
+  return Math.floor(a * b);
+}
 
 // //Rendering Function
 // function rendering(obj, title) {
